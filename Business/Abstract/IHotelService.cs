5a18d03cf902e01238432faf6ff8e528bb4de6ab
= <=>
2021-02-22 17:16:57 +0300

    Business katmanı oluşturuldu

---
+++ Business/Abstract/IHotelService.cs
@@ -0,0 +1,16 @@
+﻿using Entities;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Business.Abstract
+{
+  public  interface IHotelService
+    {
+        List<Hotel> GetAllHotels();
+        Hotel GetHotelById(int id);
+        Hotel CreateHotel(Hotel hotel);
+        Hotel UpdateHotel(Hotel hotel);
+        void Delete(int id);
+    }
+}
---
+++ Business/Concrete/HotelManager.cs
@@ -0,0 +1,49 @@
+﻿using Business.Abstract;
+using DataAccess.Abstract;
+using DataAccess.Concrete;
+using Entities;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Business.Concrete
+{
+    public class HotelManager : IHotelService
+    {
+        private IHotelRepository _hotelRepository;
+
+        public HotelManager()
+        {
+            _hotelRepository = new HotelRepository();
+        }
+        public Hotel CreateHotel(Hotel hotel)
+        {
+            return _hotelRepository.CreateHotel(hotel);
+        }
+
+        public void Delete(int id)
+        {
+            _hotelRepository.Delete(id);
+        }
+
+        public List<Hotel> GetAllHotels()
+        {
+            return _hotelRepository.GetAllHotels();
+        }
+
+        public Hotel GetHotelById(int id)
+        {
+            if (id>0)
+            {
+                return _hotelRepository.GetHotelById(id);
+            }
+            throw new Exception("id can not be less than 1");
+         
+        }
+
+        public Hotel UpdateHotel(Hotel hotel)
+        {
+            return _hotelRepository.UpdateHotel(hotel);
+        }
+    }
+}
