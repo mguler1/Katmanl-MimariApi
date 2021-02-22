eb2dd7f15e5aa1cafc4559b4e4d2fb3a0841a5ee
= <=>
2021-02-22 16:12:10 +0300

    Interface ve metodlar oluşturuldu

---
+++ DataAccess/Abstract/IHotelRepository.cs
@@ -0,0 +1,16 @@
+﻿using Entities;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DataAccess.Abstract
+{
+   public interface IHotelRepository
+    {
+        List<Hotel> GetAllHotels();
+        Hotel GetHotelById(int id);
+        Hotel CreateHotel(Hotel hotel);
+        Hotel UpdateHotel(Hotel hotel);
+        void Delete(int id)
+    }
+}
