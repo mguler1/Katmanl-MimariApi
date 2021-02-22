14421530e2c0cae8b5f2ece3e9a8497c05895ec7
= <=>
2021-02-22 15:18:03 +0300

    Proje dosyası ekle.

---
+++ DataAccess/HotelDbContext.cs
@@ -0,0 +1,18 @@
+﻿using Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DataAccess
+{
+   public class HotelDbContext:DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            base.OnConfiguring(optionsBuilder);
+            optionsBuilder.UseSqlServer("server=MEHDI;database=HotelApi;integrated security=true;");
+        }
+        public DbSet<Hotel> Hotels { get; set; }
+    }
+}
